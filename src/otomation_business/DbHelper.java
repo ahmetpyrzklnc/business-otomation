8047f0342cccb9e0315c6ae71fbdded434567dfd
Ahmet Poyraz Kılınç <ahmtklnc.software@gmail.com>
2022-10-21 15:37:41 +0300

    DbHelper Uptade

--- src/otomation_business/DbHelper.java
+++ src/otomation_business/DbHelper.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package otomation_business;
 
-/**
- *
- * @author ahmet
- */
+import java.sql.*;
+
 public class DbHelper {
+    
+    private String username = "root";
+    private String password = "1234";
+    private String dbUrl = "jdbc:mysql://127.0.0.1:3306/business_otomation";
+
+    public Connection getConnection() throws SQLException {
+        return (Connection) DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    public void showErrorMessage(SQLException exception) {
+        System.out.println("Error: " + exception.getMessage());
+        System.out.println("Error Code: " + exception.getErrorCode());
+    }
     
 }
