48a06ea950d00a04bec06f691ff7f5553fdcca69
Ahmet Poyraz Kılınç <ahmtklnc.software@gmail.com>
2022-10-21 23:54:13 +0300

    Customer Manager Uptade

--- src/otomation_business/CustomerManager.java
+++ src/otomation_business/CustomerManager.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package otomation_business;
 
-/**
- *
- * @author ahmet
- */
+
 public class CustomerManager {
-    
+    private int customer_id;
+    private String customer_name;
+    private String customer_lastname;
+
+    public CustomerManager(int customer_id, String customer_name, String customer_lastname) {
+        this.customer_id = customer_id;
+        this.customer_name = customer_name;
+        this.customer_lastname = customer_lastname;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_lastname() {
+        return customer_lastname;
+    }
+
+    public void setCustomer_lastname(String customer_lastname) {
+        this.customer_lastname = customer_lastname;
+    }
 }
