16349ccdbed856824b029aa01355559291d6b264
Ahmet Poyraz Kılınç <ahmtklnc.software@gmail.com>
2022-10-21 14:56:48 +0300

    employee screen upatde button

--- src/otomation_business/Employee_screen.java
+++ src/otomation_business/Employee_screen.java
@@ -311,8 +311,8 @@ public class Employee_screen extends javax.swing.JFrame {
     }//GEN-LAST:event_btguncelleActionPerformed
 
     private void btnAnasayfaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAnasayfaActionPerformed
-    Home_page page = new Home_page();
-    page.setVisible(true);
+      Home_page page = new Home_page();
+      page.setVisible(true);
     }//GEN-LAST:event_btnAnasayfaActionPerformed
 
     
